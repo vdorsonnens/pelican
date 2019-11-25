ec4e0107225eab4ff1b933d9e769c57e3a9649c6
yli97 <danlyh5288@gmail.com>
2019-11-24 19:10:27 -0500

    added check_modules.py, modified enable_travis.py to check dependencies

--- scripts/enable-travis.py
+++ scripts/enable-travis.py
@@ -1,74 +1,88 @@
-import os, sys
+import os
+import sys
+import subprocess
 
-from git import Repo
-from travispy import TravisPy
+subprocess.call(["python", "check_modules.py"])
+
+try:
+    from git import Repo
+except ImportError:
+    print("module not found")
+
+try:
+    from travispy import TravisPy
+except ImportError:
+    print("module not found")
+
 
 PELICAN = 'pelican'
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 TOKEN_FILE = '.travistoken'
 
+
 def get_pelican_repo():
-  # get the parent of the current directory
-  repo_path = os.path.dirname(DIR_PATH)
-  return Repo(repo_path)
+    # get the parent of the current directory
+    repo_path = os.path.dirname(DIR_PATH)
+    return Repo(repo_path)
 
 
 def get_travis_token():
-  token_path = os.path.join(DIR_PATH, TOKEN_FILE)
-  if os.path.isfile(token_path):
-    return read_token_file(token_path)
-  
-  print('You need your TravisCI token to authenticate with the API')
-  print('Find it at this address: https://travis-ci.org/account/preferences')
-  
-  token = ''
-  while token == '':
-    token = input('Your token: ')
-  
-  write_token_file(token_path, token)
+    token_path = os.path.join(DIR_PATH, TOKEN_FILE)
+    if os.path.isfile(token_path):
+        return read_token_file(token_path)
 
-  return token
+    print('You need your TravisCI token to authenticate with the API')
+    print('Find it at this address: https://travis-ci.org/account/preferences')
+
+    token = ''
+    while token == '':
+        token = input('Your token: ')
+
+    write_token_file(token_path, token)
+
+    return token
+
 
 def read_token_file(path):
-  token = ''
-  with open(path) as f:
-    token = f.read()
+    token = ''
+    with open(path) as f:
+        token = f.read()
 
-  return token
+    return token
+
 
 def write_token_file(path, token):
-  with open(path, 'w+') as f:
-    f.write(token)
+    with open(path, 'w+') as f:
+        f.write(token)
+
 
 if __name__ == '__main__':
-  # get the pelican repo and read the configuration
-  repo = get_pelican_repo()
-  reader = repo.config_reader()
-  
-  # build the remote repo name with the github user, ex: vdorsonnens/pelican
-  reader.read()
-  github_username = reader.get_value('user', 'name')
-  remote_repo = '/'.join([github_username, PELICAN])
+    # get the pelican repo and read the configuration
+    repo = get_pelican_repo()
+    reader = repo.config_reader()
 
-  # connect to travis-ci
-  token = get_travis_token()
-  travis_client = TravisPy(token)
-  travis_repo = travis_client.repo(remote_repo)
+    # build the remote repo name with the github user, ex: vdorsonnens/pelican
+    reader.read()
+    github_username = reader.get_value('user', 'name')
+    remote_repo = '/'.join([github_username, PELICAN])
 
-  success = False
-  if travis_repo.active:
-    print('Repository is already enabled')
-    sys.exit(0)
+    # connect to travis-ci
+    token = get_travis_token()
+    travis_client = TravisPy(token)
+    travis_repo = travis_client.repo(remote_repo)
 
-  else:
-    print('Enabling repo...')
-    success = travis_repo.enable()
+    success = False
+    if travis_repo.active:
+        print('Repository is already enabled')
+        sys.exit(0)
 
+    else:
+        print('Enabling repo...')
+        success = travis_repo.enable()
 
-  if success:
-    print('Operation successful')
-    sys.exit(0)
-  
+    if success:
+        print('Operation successful')
+        sys.exit(0)
 
-  print('Failed operation')
-  sys.exit(1)
+    print('Failed operation')
+    sys.exit(1)
